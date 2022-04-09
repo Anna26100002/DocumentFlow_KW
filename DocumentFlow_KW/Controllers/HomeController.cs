38e8d699140039094ea3051ec7b688c8af518f85
Anna26100002 <69114344+Anna26100002@users.noreply.github.com>
2022-04-09 20:12:54 +0300

    Update HomeController.cs

--- DocumentFlow_KW/Controllers/HomeController.cs
+++ DocumentFlow_KW/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using DocumentFlow_KW.Models;
+using DocumentFlow_KW.Models;
+using DocumentFlow_KW.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,14 +14,30 @@ namespace DocumentFlow_KW.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        UserManager<User> _userManager;
+        private readonly ApplicationContext db;
+        public HomeController(ILogger<HomeController> logger, ApplicationContext db, UserManager<User> userManager)
         {
             _logger = logger;
+            this.db = db;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> IndexAsync()
         {
+            var model = new Document
+            {
+                //Users = db.Users.ToList()
+            };
+
+            // получаем id текущего пользователя
+            var id = _userManager.GetUserId(User);
+            User user2 = await _userManager.FindByIdAsync(id);
+            //var Executor = user2.Fio;
+            //model.Executor = Executor;
+            //db.Documents.ToList();
+            //model = user.Fio;
+            //return View(db.Documents.ToList());
             return View();
         }
 
