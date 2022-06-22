5ca4b870f11e6aa1d96e887507a024abb956bacd
Anna26100002 <69114344+Anna26100002@users.noreply.github.com>
2022-06-23 01:13:05 +0300

    Update RolesController.cs

--- DocumentFlow_KW/Controllers/RolesController.cs
+++ DocumentFlow_KW/Controllers/RolesController.cs
@@ -112,13 +112,5 @@ namespace DocumentFlow_KW.Controllers
 
             return NotFound();
         }
-
-        //[HttpPost]
-        //public async Task<IActionResult> Initialize()
-        //{
-        //    await RoleInitializer.InitializeAsync(_userManager, _roleManager);
-
-        //    return RedirectToAction("Index");
-        //}
     }
 }
